b89f1aa34b16b3070ab410078b1476a2853c92f0
Kanstantsin Kulak <Kanstantsin.Kulak@nuance.com>
2019-10-23 09:55:42 +0300

    machine learning

--- src/main/java/Actions.java
+++ src/main/java/Actions.java
@@ -6,8 +6,8 @@ public class Actions {
     public static final List<Function<Integer, Integer>> FUNCTIONS = Arrays.asList(
             x -> x + 1,
             x -> 0,
+            x -> x * 100,
             x -> x % 2,
-            x -> x / 2,
-            x -> x * 100
+            x -> x / 2
     );
 }
