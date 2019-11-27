07bdb52aabd6a4e33d44528bf425117f0910b4af
Kanstantsin Kulak <Kanstantsin.Kulak@nuance.com>
2019-11-27 15:44:09 +0300

    fix dfs

--- src/main/java/Decomp.kt
+++ src/main/java/Decomp.kt
@@ -1,52 +1,80 @@
 package solution
 
-import kotlin.math.sqrt
-
 object Decomp {
+    private val trees = mutableListOf(Tree(0, square = Long.MAX_VALUE))
+
+    init {
+        trees.add(Tree(1, mutableListOf(trees[0])))
+    }
 
     fun decompose(n: Long): String {
         val result = StringBuilder()
         if (n in 0L..4L) {
             result.append("null")
         } else {
-            val map = mutableMapOf<Long, Long>()
-            decompose(map, n)
-            map.keys.sorted().forEach { result.append(it).append(" ") }
+            val tree = createTree(n)
+            tree.search().filter { it.key != n }.map { it.key }.sorted().forEach { result.append(it).append(" ") }
         }
-        return result.replace(Regex("\\s\$"), "")
+        return result.toString().replace(Regex("\\s\$"), "")
     }
 
-    fun decompose(map: MutableMap<Long, Long>, n: Long): Long {
-        var result = 0L
-        if (n == 0L) {
-            result = 0L
-        } else if (n in 1L..4L) {
-            result = n
+    fun createTree(key: Long): Tree = if (key == 0L) {
+        trees[0]
+    } else if (key == 1L) {
+        trees[1]
+    } else {
+        val tree = Tree(key)
+        for (i in 0 until key) {
+            if (trees.size < i + 1) {
+                trees.add(createTree(i))
+            }
+            tree.leafs.add(trees[i.toInt()])
+        }
+        tree
+    }
+}
+
+class Tree(var key: Long, var leafs: MutableList<Tree> = mutableListOf(), val square: Long = key * key, var visited: Boolean = false, var left: Long = square) {
+    fun search(): List<Tree> {
+        leafs.sortWith(TreeComparator())
+        val result = mutableListOf<Tree>()
+        if (leafs.isNotEmpty()) {
+            loop@ for (leaf in leafs) {
+                if (!leaf.visited) {
+                    result.add(this)
+                    leaf.visited = true
+                    leaf.left = left - leaf.square
+                    when {
+                        leaf.left == 0L -> {
+                            result.add(leaf)
+                            break@loop
+                        }
+                        leaf.left > 0L -> {
+                            val list = leaf.search()
+                            if (list.isNotEmpty()) {
+                                result.addAll(list)
+                                break@loop
+                            } else {
+                                leaf.leafs.forEach { it.visited = false }
+                                result.clear()
+                            }
+                        }
+                        else -> {
+                            leaf.leafs.forEach { it.visited = false }
+                            result.clear()
+                        }
+                    }
+                }
+            }
         } else {
-            decompose(map, n, n)
+            result.add(this)
         }
         return result
     }
-
-    fun decompose(map: MutableMap<Long, Long>, n: Long, i: Long) {
-        map[i - 1] = n dif mutableMapOf(i - 1 to 0L)
-        var dif = n dif map
-        while (dif != 0L) {
-            map[dif] = dif
-            dif = n dif map
-            if (map.containsKey(dif)) {
-
-            }
-        }
-    }
-
-    fun Long.sqrt() = sqrt(this.toDouble()).toLong()
-
-    fun Long.n2() = this * this
-
-    infix fun Long.dif(map: MutableMap<Long, Long>) = (this.n2() - map.keys.map { it.n2() }.sum()).sqrt()
 }
 
-fun main() {
-    println(Decomp.decompose(50))
+class TreeComparator : Comparator<Tree> {
+    override fun compare(o1: Tree?, o2: Tree?): Int {
+        return o2!!.key.compareTo(o1!!.key)
+    }
 }
