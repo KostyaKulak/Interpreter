b1c1f75a4f77fb2692e15674064e255168a5a0b3
Kanstantsin Kulak <Kanstantsin.Kulak@nuance.com>
2019-11-28 12:08:00 +0300

    fix heap error

--- src/main/java/Kata.java
+++ src/main/java/Kata.java
@@ -1,39 +1,27 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Kata {
 
     public static List<Point> closestPair(List<Point> points) {
-        List<PointExt> pointExts = new ArrayList<>();
+        Map<Double, List<Point>> pointMap = new HashMap<>();
         for (int i = 0; i < points.size(); i++) {
             for (int j = i + 1; j < points.size(); j++) {
-                pointExts.add(new PointExt(i, j, Math.sqrt(Math.pow(points.get(i).x - points.get(j).x, 2) + Math.pow(points.get(i).y - points.get(j).y, 2))));
+                Point iPoint = points.get(i);
+                Point jPoint = points.get(j);
+                double d = Math.sqrt(Math.pow(iPoint.x - jPoint.x, 2) + Math.pow(iPoint.y - jPoint.y, 2));
+                List<Point> pointList = pointMap.getOrDefault(d, new ArrayList<>());
+                pointList.addAll(Arrays.asList(iPoint, jPoint));
+                pointMap.put(d, pointList);
             }
         }
-        double min = pointExts.stream()
-                .min(Comparator.comparingDouble(o -> o.d))
-                .map(pointExt -> pointExt.d)
-                .orElse(Double.MAX_VALUE);
-        return pointExts.stream()
-                .filter(pointExt -> pointExt.d == min)
-                .flatMap(pointExt -> {
-                    List<Point> p = new ArrayList<>();
-                    p.add(points.get((int) pointExt.x));
-                    p.add(points.get((int) pointExt.y));
-                    return p.stream();
-                })
+        double min = pointMap.keySet().stream().min(Comparator.naturalOrder()).orElse(Double.MAX_VALUE);
+        return pointMap.entrySet()
+                .stream()
+                .filter(pointExt -> pointExt.getKey() == min)
+                .map(Map.Entry::getValue)
+                .flatMap(Collection::stream)
                 .collect(Collectors.toList());
 
-    }
-
-    private static class PointExt extends Point {
-        private double d;
-
-        public PointExt(double x, double y, double d) {
-            super(x, y);
-            this.d = d;
-        }
     }
 }
